73ccf5cdfca2bc0e36d2fca95fe4effa44d67572
Raymond Hammarling <raykay.rh@gmail.com>
2014-04-16 03:30:25 +0200

    Added a couple more choices

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
+var npm = require('npm');
 
 function nodeModuleName(filePath) {
 	var basename = path.basename(filePath);
@@ -41,6 +42,18 @@ var NodeModuleGenerator = yeoman.generators.Base.extend({
 				default: nodeModuleName(process.cwd())
 			},
 			{
+				type: 'input',
+				name: 'description',
+				message: 'What is its description?',
+				default: 'Out of the league!'
+			},
+			{
+				type: 'input',
+				name: 'author',
+				message: 'Who is the author?',
+				default: npm.config.get('init.author.name')
+			},
+			{
 				type: 'list',
 				name: 'language',
 				message: 'Which language should this module be written in?',
@@ -48,11 +61,18 @@ var NodeModuleGenerator = yeoman.generators.Base.extend({
 					{ name: 'JavaScript', value: 'js' },
 					{ name: 'CoffeeScript', value: 'coffee' }
 				]
+			},
+			{
+				type: 'confirm',
+				name: 'publishSource',
+				message: 'Should the original source be included as well when publishing to the npm registry?',
+				default: false,
+				when: function(answers) { return answers.language === 'coffee'; }
 			}
 		];
 
-		this.prompt(prompts, function (props) {
-			for(var k in props) {
+		this.prompt(prompts, function(answers) {
+			for(var k in answers) {
 				this[k] = props[k];
 			}
 
